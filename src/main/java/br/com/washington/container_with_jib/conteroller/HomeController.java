771481fd5967dcf4f41b7c6b6823e814802ceaa2
Washington <washington195luiz@gmail.com>
2025-04-27 18:49:03 -0300

    Create pipeline to build image and send to oracle and create a simple http response to get request

---
+++ src/main/java/br/com/washington/container_with_jib/conteroller/HomeController.java
@@ -0,0 +1,13 @@
+package br.com.washington.container_with_jib.conteroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home(){
+        return "Hello World!";
+    }
+}
