9cf42ac40605df4937238eec46bb29c40b2d9d70
Washington <washington195luiz@gmail.com>
2025-04-27 18:13:36 -0300

    First commit

---
+++ src/main/java/br/com/washington/container_with_jib/ContainerWithJibApplication.java
@@ -0,0 +1,13 @@
+package br.com.washington.container_with_jib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerWithJibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerWithJibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/washington/container_with_jib/ContainerWithJibApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.washington.container_with_jib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerWithJibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
