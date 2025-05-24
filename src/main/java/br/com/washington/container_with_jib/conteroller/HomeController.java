841130b6b09b84685ab94371067530aaddd801c7
Washington <washington195luiz@gmail.com>
2025-05-24 00:11:25 -0300

    again

--- src/main/java/br/com/washington/container_with_jib/conteroller/HomeController.java
+++ src/main/java/br/com/washington/container_with_jib/conteroller/HomeController.java
@@ -4,6 +4,7 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 public class HomeController {
 
