70f1f5193919dcbd0d425a21786e660f972e5ac5
Washington <washington195luiz@gmail.com>
2025-05-23 23:10:21 -0300

    testing

--- src/main/java/br/com/washington/container_with_jib/conteroller/HomeController.java
+++ src/main/java/br/com/washington/container_with_jib/conteroller/HomeController.java
@@ -1,13 +1,38 @@
 package br.com.washington.container_with_jib.conteroller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class HomeController {
 
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    @Value("${value}")
+    private String value;
+
+    @CircuitBreaker(name = "Test", fallbackMethod = "fallbackCircuitBreak")
+    @GetMapping("/test")
+    public String test(){
+        if(true) throw new RuntimeException();
+        return "a";
+    }
+
     @GetMapping
     public String home(){
-        return "Last update: 05/03/2025";
+        return value;
+    }
+
+    private String fallbackCircuitBreak(RuntimeException e){
+        log.error("Error");
+        return "Error ";
     }
 }
