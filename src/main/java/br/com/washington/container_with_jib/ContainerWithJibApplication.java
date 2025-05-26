dc640f87997a2978f66dda07f7dab1f5869c32dc
Washington <washington195luiz@gmail.com>
2025-05-26 14:19:22 -0300

    updating test 3

--- src/main/java/br/com/washington/container_with_jib/ContainerWithJibApplication.java
+++ src/main/java/br/com/washington/container_with_jib/ContainerWithJibApplication.java
@@ -2,8 +2,10 @@ package br.com.washington.container_with_jib;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ContainerWithJibApplication {
 
